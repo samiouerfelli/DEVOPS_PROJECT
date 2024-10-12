62db4b05c36d4c7e3fbbd7e65d8af8db5ebd816c
Sami Ouerfelli <sami.ouerfelli@esprit.tn>
2024-10-12 18:11:54 +0100

    initial-commit

---
+++ src/main/java/tn/esprit/tpfoyer/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.tpfoyer.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esprit/tpfoyer/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.tpfoyer.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
