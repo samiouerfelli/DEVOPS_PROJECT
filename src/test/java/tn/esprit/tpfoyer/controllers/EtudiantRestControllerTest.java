b4b8a806a6c691c2112532f66b7a17acd03b112a
Melek Bejaoui <melekbejaoui29@gmail.com>
2024-11-01 19:35:05 +0100

    Add Tests #A v2

--- src/test/java/tn/esprit/tpfoyer/controllers/EtudiantRestControllerTest.java
+++ src/test/java/tn/esprit/tpfoyer/controllers/EtudiantRestControllerTest.java
@@ -1,4 +1,4 @@
-package controller;
+package tn.esprit.tpfoyer.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
--- src/test/java/tn/esprit/tpfoyer/services/EtudiantServiceImplTest.java
+++ src/test/java/tn/esprit/tpfoyer/services/EtudiantServiceImplTest.java
@@ -1,4 +1,4 @@
-    package service;
+package tn.esprit.tpfoyer.services;
 
     import org.junit.jupiter.api.BeforeEach;
     import org.junit.jupiter.api.Test;
