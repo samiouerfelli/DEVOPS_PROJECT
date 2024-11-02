a437a243c913010ffec3c8e30d98b2cd352e23b6
Louay <93862698+Loukh1@users.noreply.github.com>
2024-11-02 22:50:37 +0100

    Update BlocRepository.java

--- src/main/java/tn/esprit/tpfoyer/repository/BlocRepository.java
+++ src/main/java/tn/esprit/tpfoyer/repository/BlocRepository.java
@@ -8,87 +8,19 @@ import tn.esprit.tpfoyer.entity.Bloc;
 import java.util.List;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 @Repository
  public interface BlocRepository extends JpaRepository<Bloc, Long> {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   // Récupérer les Blocs qui ont une capacité supérieure à 50 :
 
    List<Bloc> findAllByCapaciteBlocGreaterThan(long c);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   // Récupérer les Blocs qui ont une capacité inférieurs à 50 :
-   // List<Bloc> findAllByCapaciteBlocLessThan(long c);
-
-
     // Récupérer tous les Blocs qui ont un nom qui commence par "Bl" :
     List<Bloc> findAllByNomBlocStartingWith(String c);
-
-
     // Récuprer tous les blocs qui ont un nom donné et une capacité donnée :
     List<Bloc> findAllByNomBlocAndCapaciteBloc (String nom , long capacite );
-
-
     // Récupérer le bloc qui a un nom donné :
     Bloc findByNomBloc (String nom);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     /* No need to code CRUD here. Its is already in the
     interfaces provided by the framework Spring Data JPA :
@@ -102,12 +34,6 @@ import java.util.List;
 
     // List des blocs non affectés à aucun foyer :
     List<Bloc> findAllByFoyerIsNull();
-
-
-    //Bloc findByC
-    //findAllByFoyerIsNull();
-
-
 
 
 }
