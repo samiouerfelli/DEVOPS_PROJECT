9ab9ed8d2d69d57fe746d49dd2c4249fd30b1bb6
Louay <93862698+Loukh1@users.noreply.github.com>
2024-11-02 23:07:33 +0100

    Update BlocTest.java

--- src/test/java/tn/esprit/tpfoyer/entities/BlocTest.java
+++ src/test/java/tn/esprit/tpfoyer/entities/BlocTest.java
@@ -11,7 +11,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class BlocTest {
-   private static final String BLOC = "Bloc A";
+   private static final String BLOC_A = "Bloc A";
 
     private Bloc bloc;
 
