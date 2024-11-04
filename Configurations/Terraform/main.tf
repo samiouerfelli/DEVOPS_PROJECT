bbf12f438f9bee3d2fc4ff06223392ce73da06e6
Sami Ouerfelli <sami.ouerfelli@esprit.tn>
2024-11-04 12:43:36 +0100

    Add Config Folder

---
+++ Configurations/Terraform/main.tf
@@ -0,0 +1,172 @@
+provider "azurerm" {
+  features {}
+  subscription_id = var.azure_subscription_id
+}
+
+resource "azurerm_resource_group" "DevOps_rg" {
+  name     = "DevOps-resource-group"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "devops-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.DevOps_rg.location
+  resource_group_name = azurerm_resource_group.DevOps_rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "devops-subnet"
+  resource_group_name  = azurerm_resource_group.DevOps_rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg_ssh" {
+  name                = "devops-nsg-ssh"
+  location            = azurerm_resource_group.DevOps_rg.location
+  resource_group_name = azurerm_resource_group.DevOps_rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "0.0.0.0/0"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-Port-9000"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "9000"
+    source_address_prefix      = "0.0.0.0/0"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-Port-8080"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "0.0.0.0/0"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-Port-8081"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8081"
+    source_address_prefix      = "0.0.0.0/0"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_public_ip" "vm_pip" {
+  name                = "vm-public-ip"
+  location            = azurerm_resource_group.DevOps_rg.location
+  resource_group_name = azurerm_resource_group.DevOps_rg.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "vm_nic" {
+  name                = "vm-nic"
+  location            = azurerm_resource_group.DevOps_rg.location
+  resource_group_name = azurerm_resource_group.DevOps_rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_pip.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "vm_nsg_association" {
+  network_interface_id      = azurerm_network_interface.vm_nic.id
+  network_security_group_id = azurerm_network_security_group.nsg_ssh.id
+}
+
+variable "ssh_public_key_path" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "devops-vm"
+  location              = azurerm_resource_group.DevOps_rg.location
+  resource_group_name   = azurerm_resource_group.DevOps_rg.name
+  network_interface_ids = [azurerm_network_interface.vm_nic.id]
+  vm_size               = "Standard_D4s_v3"
+
+  storage_os_disk {
+    name              = "vm-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "devops-vm"
+    admin_username = "manager"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+    ssh_keys {
+      path     = "/home/manager/.ssh/authorized_keys"
+      key_data = file(var.ssh_public_key_path)
+    }
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+  name                = "devops-k8s-cluster"
+  location            = azurerm_resource_group.DevOps_rg.location
+  resource_group_name = azurerm_resource_group.DevOps_rg.name
+  dns_prefix          = "k8s-cluster"
+
+  default_node_pool {
+    name       = "agentpool"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "additional_node_pool" {
+  name                  = "aks-pubcnodepool"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.k8s.id
+  vm_size               = "Standard_B2s"
+  node_count            = 1
+  enable_node_public_ip = true
+}
---
+++ Configurations/Terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "azure_subscription_id" {
+  description = "The Azure subscription ID."
+  type        = string
+}
