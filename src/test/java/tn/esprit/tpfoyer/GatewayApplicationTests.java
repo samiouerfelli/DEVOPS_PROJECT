d5d1a3559a87838fa5d3369e24d09f3559788faf
Sami Ouerfelli <sami.ouerfelli@esprit.tn>
2024-11-04 13:12:18 +0100

    Improve Maintainability

--- src/test/java/tn/esprit/tpfoyer/GatewayApplicationTests.java
+++ src/test/java/tn/esprit/tpfoyer/GatewayApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class GatewayApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() { // Test if the context loads
 	}
 
 }
